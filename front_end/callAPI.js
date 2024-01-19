d83c7c824afc680f3cc2dafc7fe2487869ce56d2
efundamentals-manhnguyen <manh.nguyenvan@efundamentals.com>
2024-01-19 16:58:00 +0700

    dispaly_author_and_book_api_data_to_front_end

--- front_end/callAPI.js
+++ front_end/callAPI.js
@@ -1,7 +1,19 @@
 let getBookApi = "http://127.0.0.1:5000/book-management/books"
+let getCategoryApi = "http://127.0.0.1:5000/category-management/categories"
+let getAuthorApi = "http://127.0.0.1:5000/author-management/authors"
 
 async function loadBooks(){
     let listBooks = await fetch(getBookApi).then(res => res.json());
     return listBooks;
+}
+
+async function loadCategories(){
+    let listCategories = await fetch(getCategoryApi).then(res => res.json());
+    return listCategories
+}
+
+async function loadAuthors(){
+    let listAuthors= await fetch(getAuthorApi).then(res => res.json());
+    return listAuthors
 }
 
